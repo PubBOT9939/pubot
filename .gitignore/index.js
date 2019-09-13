377b4cd4da7c81a0074880b4867fd735d60ba4dc
PubBOT9939 <55290858+PubBOT9939@users.noreply.github.com>
2019-09-13 18:32:15 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var prefix = ("p;")
+
+
+client.on('message', (message) => {
+
+	if(message.content === "bonjour") {
+		message.channel.send("Salut! ")
+	}
+
+	if(message.content === prefix + "help"){
+		message.channel.send("Veux-tu de l'aide?")
+	}
+})
+
+client.login("NjIxMzM4NzQ5NTU2NDkwMjYw.XXj7ow.hCfsF32DEb5XQLqhIX9e6AdGJ5s")
