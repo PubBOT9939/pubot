496899df2f152cbfdbf35583007a2ffe53a41c65
PubBOT9939 <55290858+PubBOT9939@users.noreply.github.com>
2019-09-25 23:43:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -146,19 +146,20 @@ client.on("message", message => {
 		let args = messageArray.slice(1);
 
 		let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if(!rUser) return message.channel.send("Couldn't find user.");
+		if(!rUser) return message.channel.send("Utilisateur introuvable.");
 
 		// Reason of the report
 		let reason = args.join(" ").slice(22);
+		if(!reason) return message.channel.send
 
 		let reportEmbed = new Discord.RichEmbed()
 		.setDescription("Reports")
 		.setColor("#15f153")
-		.addField("Reported User", rUser + " with ID: ``" + rUser.id + "``")
-		.addField("Reported By", message.author + " with ID: ``" + message.author.id + "``")
+		.addField("Utilisateur report", rUser + " avec l'ID: ``" + rUser.id + "``")
+		.addField("Report par", message.author + " avec l'ID: ``" + message.author.id + "``")
 		.addField("Channel", message.channel)
-		.addField("Time", message.createdAt)
-		.addField("Reason", `${reason}`);
+		.addField("A", message.createdAt)
+		.addField("Raison", `${reason}`);
 
 		let reportsChannel = message.guild.channels.find("name", "reports");
 		if(!reportsChannel) return message.channel.send("Couldn't find reports channel");
